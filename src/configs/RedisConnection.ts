67dd6ce639d3c178b40d26a262d7a8e7b254511a
Alej212 <alejandrocevedo09092002@gmail.com>
2024-05-28 21:10:49 -0400

    feat: :card_file_box: connection to redis established

--- src/configs/RedisConnection.ts
+++ src/configs/RedisConnection.ts
@@ -1,20 +1,18 @@
-import * as redis from 'redis'
-import dotenv from 'dotenv'
-dotenv.config()
+// import { createClient } from 'redis'
+// import dotenv from 'dotenv'
+// dotenv.config()
 
-const client = redis.createClient({
-  url: 'redis://localhost:6379',
-  password: process.env.REDIS_PASSWORD
+// const redis = createClient({
+//   url: 'redis://redis:6379',
+//   password: 'password'
+// })
+
+import { Redis } from 'ioredis'
+
+const redis = new Redis({
+  host: 'redis',
+  port: 6379,
+  password: 'password'
 })
 
-// Escucha el evento 'ready'
-client.on('ready', () => {
-  console.log('Conexión a Redis establecida correctamente')
-})
-
-// Escucha el evento 'error' para manejar posibles errores
-client.on('error', (err) => {
-  console.error('Error en la conexión a Redis:', err)
-})
-
-export default client
+export default redis
